91e47c91e6a6d744a6fd4eb75d63d634a61484b2
nourharidy <nourharidy@yahoo.com>
2018-03-29 19:12:14 +0200

    Added events

--- rockPS.sol
+++ rockPS.sol
@@ -6,6 +6,10 @@ contract RSP {
         bool played;
         uint8 move;
     }
+    event Submitted(address Player, bytes32 hash);
+    event Winner(address Player, uint256 prize, uint8 move);
+    event Draw(uint8 move);
+    event Revealed(address Player, uint8 move, string nonce);
     mapping (address => Player) public players;
     uint public bet;
     address player1; address player2;
@@ -33,37 +37,35 @@ contract RSP {
         Player storage thisPlayer = players[msg.sender];
         thisPlayer.hash = _hash;
         thisPlayer.played = true;
+        emit Submitted(msg.sender, _hash);
         return true;
+        
     }
     //1: rock, 2: paper, 3: scissors
-    function reveal(uint8 _move, string nonce) public onlyPlayers validMove(_move) returns (string){
+    function reveal(uint8 _move, string nonce) public onlyPlayers validMove(_move) {
         require(players[msg.sender].move == 0);
         require(players[msg.sender].hash == keccak256(_move, nonce));
         players[msg.sender].move = _move;
-        if(players[player1].move > 0 && players[player2].move > 0){
-            if(players[player1].move == 1 && players[player2].move == 3){
+        emit Revealed(msg.sender, _move, nonce);
+        if((players[player1].move > 0 && players[player2].move > 0)){
+            if((players[player1].move == 1 && players[player2].move == 3)
+                || (players[player1].move == 3 && players[player2].move == 2)
+                || (players[player1].move == 2 && players[player2].move == 1) 
+            ){
                 player1.transfer(bet*2);
-                return "Player 1.";
-            }else if(players[player1].move == 2 && players[player2].move == 1){
-                player1.transfer(bet*2);
-                return "Player 1.";
-            }else if(players[player1].move == 3 && players[player2].move == 2){
-                player1.transfer(bet*2);
-                return "Player 1 won.";
+                emit Winner(player1, bet*2, players[player1].move);
             }else if(players[player1].move == players[player2].move){
                 player1.transfer(bet);
                 player2.transfer(bet);
-                return "Draw";  
+                emit Draw(players[player1].move);
             }else{
                 player2.transfer(bet*2);
-                return "Player 2 won ya .";
+                emit Winner(player2, bet*2, players[player2].move);
             }
             //Reset
             players[player1] = Player(true, "", false, 0);
             players[player2] = Player(true, "", false, 0);
-        }else{
-            return "Well played.";
         }
-        return "Well played";
     }
 }
+
